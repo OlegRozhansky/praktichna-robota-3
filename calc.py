f849d08039cd5f32a8265afe942970a022c51889
OlegRozhansky <oleg.rozhansky@gmail.com>
2023-11-10 01:19:16 +0200

    add srv-class and tests

---
+++ calc.py
@@ -0,0 +1,4 @@
+class Calc:
+
+    def sum(self, a: int, b: int) -> int:
+        return a + b
---
+++ calc_test.py
@@ -0,0 +1,15 @@
+import unittest
+from calc import Calc
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(Calc().sum(1, 2), 3)
+
+    def test_sum2(self):
+        self.assertEqual(Calc().sum(1, 2), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
